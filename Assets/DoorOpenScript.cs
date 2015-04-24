c40be455463efd50096eaf5f4e70e9d0e604a8d1
candaya <christopher.t.andaya@gmail.com>
2015-04-24 14:23:47 -0700

    initial team assests commit

---
+++ Assets/DoorOpenScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpenScript : MonoBehaviour {
+
+	public int iDirection;
+	public int startPos;
+	public int maxDistance;
+	Vector3 pos;
+	Vector3 vel;
+	
+	void Update () 
+	{
+		if(iDirection==1)
+			if(pos[0]<iDirection*maxDistance)
+		{
+			transform.Translate(Vector3.right * iDirection * Time.deltaTime);
+			pos = transform.position;
+		}
+		
+		if(iDirection==-1)
+			if(pos[0]>iDirection*maxDistance)
+		{
+			transform.Translate(Vector3.right * iDirection * Time.deltaTime);
+			pos = transform.position;
+		}
+	}
+}
