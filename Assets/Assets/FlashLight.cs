a6b11d33cb0fabef76fb6032a0a71db3d7eb51b3
candaya <christopher.t.andaya@gmail.com>
2015-05-05 22:24:24 -0700

    modified pause script to use 'P' instead of 'esc'. added background sound.
    if this commit messes everything up, then it needs to roll back. this is a lot of changes im not sure where they came from. if this becomes broken use the other morning_star repo.

---
+++ Assets/Assets/FlashLight.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashLight : MonoBehaviour 
+{
+	Light flashlight;
+	bool on = false;
+	
+	void Start()
+	{
+		flashlight = GetComponentInChildren<Light>();
+	}
+	
+	void Update()
+	{
+		if(on)
+			flashlight.GetComponent<Light>().enabled = true;
+		else if(!on)
+			flashlight.GetComponent<Light>().enabled = false;
+		if(Input.GetKeyDown(KeyCode.F))
+			on = !on;
+	}
+}
